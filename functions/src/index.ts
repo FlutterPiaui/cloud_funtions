dd3b844a7302497db3eb7c9811108d1be59bc1ba
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-07-08 10:54:31 -0300

    v1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,36 +1,29 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// import { logger } from "firebase-functions/v1";
-import { onRequest, Request } from "firebase-functions/v2/https";
-
+import {onRequest} from "firebase-functions/v2/https";
+import * as express from "express";
 import fetch from "node-fetch";
 
-const express = require("express");
-
-const app = express();
+const app = express.default();
 
 const baseUrl = "https://api.themoviedb.org/3";
+const apiKey = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YmM3
+NjhkY2YzMmZmZTZmMTMzZTVkMDhlMDg5MTgyNiIsIm5iZiI6MTcyMDI2NDc2NC4xM
+DAyMzIsInN1YiI6IjY2ODkyNjY2MjBlODcyZGE2NmEwNjIxMiIsInNjb3BlcyI6WyJh
+cGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u01Mjer3_
+VU9NLR867c8HFpWarF5BZ2XYjDPbBB2TWc`.replace(/\n/g, "");
 
 const getMovieId = async (movieName: string) => {
-  const url = `${baseUrl}/search/movie?query=${movieName}&include_adult=true&language=en-US&page=1`;
+  const url = `${baseUrl}/search/movie?query=${movieName}
+  &include_adult=true&language=en-US&page=1`.replace(/\n/g, "");
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YmM3NjhkY2YzMmZmZTZmMTMzZTVkMDhlMDg5MTgyNiIsIm5iZiI6MTcyMDI2NDc2NC4xMDAyMzIsInN1YiI6IjY2ODkyNjY2MjBlODcyZGE2NmEwNjIxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u01Mjer3_VU9NLR867c8HFpWarF5BZ2XYjDPbBB2TWc",
+      Authorization: apiKey,
     },
   };
   const res = await fetch(url, options);
-
   const data = await res.json();
+
   const id = data["results"][0]["id"];
 
   return id;
@@ -42,21 +35,23 @@ const getMovieDetails = async (movieId: string) => {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YmM3NjhkY2YzMmZmZTZmMTMzZTVkMDhlMDg5MTgyNiIsIm5iZiI6MTcyMDI2NDc2NC4xMDAyMzIsInN1YiI6IjY2ODkyNjY2MjBlODcyZGE2NmEwNjIxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u01Mjer3_VU9NLR867c8HFpWarF5BZ2XYjDPbBB2TWc",
+      Authorization: apiKey,
     },
   };
   const res = await fetch(url, options);
-
   const movieData = await res.json();
 
   return movieData;
 };
 
-app.get("/:movieName", async (req: Request, res: any) => {
-  const id = await getMovieId(req.params.movieName);
-  const movieData = await getMovieDetails(id);
-  return res.send(movieData);
+app.get("/:movieName", async (req: express.Request, res: express.Response) => {
+  try {
+    const id = await getMovieId(req.params.movieName);
+    const movieData = await getMovieDetails(id);
+    res.send(movieData);
+  } catch (error) {
+    res.status(500).send({error: "Something went wrong"});
+  }
 });
 
 export const movieDetails = onRequest(app);
