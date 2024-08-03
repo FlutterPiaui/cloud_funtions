c3008e209543b52849d195272bcfa74e08af000b
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-08-03 17:12:19 -0300

    pass chosen language in body:

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import {onRequest} from "firebase-functions/v2/https";
 import * as express from "express";
 import fetch from "node-fetch";
 import {GoogleGenerativeAI} from "@google/generative-ai";
-import {logger} from "firebase-functions/v1";
+// import { logger } from "firebase-functions/v1";
 
 const app = express.default();
 app.use(express.json());
@@ -14,12 +14,25 @@ DAyMzIsInN1YiI6IjY2ODkyNjY2MjBlODcyZGE2NmEwNjIxMiIsInNjb3BlcyI6WyJh
 cGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u01Mjer3_
 VU9NLR867c8HFpWarF5BZ2XYjDPbBB2TWc`.replace(/\n/g, "");
 
-const formatPrompt = (prompt: string) => {
-  return `"${prompt}". Please provide the titles of the
-   movies, post of the movies, description of the movies and where to watch them in JSON format like this '{"result":true, "count":42}' without this
+const languageChoicePrompt = (language: string) =>
+  `The language chosen was ${language}`;
+
+const defaultPrompt = () =>
+  `. Please provide the titles of the
+   movies in english, titles of the movies in
+  the chosen language, post of the movies in
+  the chosen language, description in
+  the chosen language
+  of the movies and where to watch them in JSON format
+   like this '{"result":true, "count":42}' without this
    \`\`\`json in the beginning and this \`\`\` in the end. The
-   JSON object should have the keys "title", "streaming_platform", "image", "description"
-   If a movie is not available on a streaming platform, please indicate "Not Available".`;
+   JSON object should have the keys
+    "englishTitle", "title", "streaming_platform", "image", "description"
+   If a movie is not available on a streaming platform,
+   please indicate "Not Available".`;
+
+const formatPrompt = (prompt: string, language: string) => {
+  return `${prompt}${defaultPrompt()}${languageChoicePrompt(language)}`;
 };
 
 const getMovieId = async (movieName: string) => {
@@ -51,9 +64,8 @@ const getMovieDetails = async (movieId: string) => {
   const res = await fetch(url, options);
   const movieData = await res.json();
 
-
   if (movieData.poster_path) {
-    movieData.poster_url = `https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movieData.poster_path}`;
+    movieData.poster_url = `https://image.tmdb.org/t/p/w500/${movieData.poster_path}`;
   } else {
     movieData.poster_url = "https://example.com/default-poster.jpg";
   }
@@ -67,7 +79,7 @@ app.get(
     try {
       const id = await getMovieId(req.params.movieName);
       const movieData = await getMovieDetails(id);
-      res.json(movieData); // Agora inclui a URL completa do pôster
+      res.json(movieData);
     } catch (error) {
       res.status(500).send({error: "Something went wrong"});
     }
@@ -80,7 +92,7 @@ const sendPromptToGemini = async (prompt: string) => {
 
   const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
   const result = await model.generateContent(prompt);
-  const response = await result.response;
+  const response = result.response;
   const text = response.text();
 
   return text;
@@ -89,10 +101,12 @@ const sendPromptToGemini = async (prompt: string) => {
 app.post("/gemini", async (req: express.Request, res: express.Response) => {
   try {
     const prompt = req.body.prompt;
-    const geminiResponse = await sendPromptToGemini(formatPrompt(prompt));
-    logger.log(JSON.parse(geminiResponse));
+    const language = req.body.language;
+    const geminiResponse = await sendPromptToGemini(
+      formatPrompt(prompt, language)
+    );
 
-    res.json(JSON.parse(geminiResponse));
+    res.json(JSON.parse(geminiResponse.slice(7, -3)));
   } catch (error) {
     res.status(500).send({error: "Something went wrong"});
   }
